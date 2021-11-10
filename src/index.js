2be473f6894f2abf5b78149d7699c803f802ec91
Samuel Taplin <samtaplin@berkeley.edu>
2021-11-09 19:55:56 -0800

    for site

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import Projects from "./projects";
 import Contact from "./contact";
 
 ReactDOM.render(
-  <BrowserRouter basename="/personalsite">
+  <BrowserRouter>
        <Switch>
         <Route exact path="/" component={Home} />
         <Route path="/blog" component={Blog} />
